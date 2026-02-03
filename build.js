ef5b8fdc8d55159d847f22e25fbcab90eb5ab005
Blankal <120738258+Blankal@users.noreply.github.com>
2026-02-02 20:35:45 -0800

    Revert "Correction to linux executable capability"
    This reverts commit 13d443a6f60e008e47fd11268e69c5efc48d3da2.
    
    	deleted:    build.js
    	new file:   dist/bundle.json
    	modified:   package.json
    	deleted:    sea-config.json

--- build.js
+++
@@ -1,9 +0,0 @@
-require('esbuild').buildSync({
-  entryPoints: ['src/index.js'],
-  bundle: true,
-  platform: 'node',
-  target: 'node22',
-  format: 'cjs',
-  outfile: 'dist/bundle.cjs',
-  external: []  // Bundles ws fine as pure JS
-});
